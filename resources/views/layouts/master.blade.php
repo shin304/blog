96462c78a0dd84599cc437020fa7f7af97fb1967
s.oono <s.oono@asto-system.com>
2021-02-28 03:38:25 +0900

    create  layout master header footer

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+  <head>
+      <meta charset="UTF-8">
+      <title>@yield('title')</title>
+      <link rel="stylesheet" href="/css/app.css">
+      <script src="/js/app.js" defer></script>
+  </head>
+  <body>
+    <header>
+      @include('layouts.header')
+    </header>
+    <br>
+    <div class="container">
+      @yield('content')
+    </div>
+    <footer>
+      @include('layouts.footer')
+    </footer>
+  </body>
+</html>
